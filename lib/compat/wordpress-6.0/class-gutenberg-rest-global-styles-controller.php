2c4f36b1a8d209c199647def7e496d8ae6d52dcd
Riad Benguella <benguella@gmail.com>
2022-01-18 11:26:50 +0100

    fix phpcs

--- lib/compat/wordpress-6.0/class-gutenberg-rest-global-styles-controller.php
+++ lib/compat/wordpress-6.0/class-gutenberg-rest-global-styles-controller.php
@@ -44,7 +44,7 @@ class Gutenberg_REST_Global_Styles_Controller extends WP_REST_Global_Styles_Cont
 	 * @param WP_REST_Request $request Full details about the request.
 	 * @return true|WP_Error True if the request has read access for the item, WP_Error object otherwise.
 	 */
-	public function get_theme_items_permissions_check( $request ) {
+	public function get_theme_items_permissions_check( $request ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
 		// Verify if the current user has edit_theme_options capability.
 		// This capability is required to edit/view/delete templates.
 		if ( ! current_user_can( 'edit_theme_options' ) ) {
